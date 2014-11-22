0a8b7791a787b16be7a6cb1bb01f1897958b9655
Akash Mehra <mehra.akash@gmail.com>
2014-11-21 23:51:27 -0800

    hiding uneeded variables that are not needed outside the module.

--- mnist.py
+++ mnist.py
@@ -2,12 +2,13 @@ import numpy as np
 import struct, gzip, os, sys
 from collections import namedtuple
 
-data = namedtuple('data',['X','Y'])
-datadir = os.path.join(os.path.dirname(__file__),'data')
+_data = namedtuple('data',['X','Y'])
+_datadir = os.path.join(os.path.dirname(__file__),'data')
 
 def _read_header(filename, fmt, numBytes):
 	f = gzip.GzipFile(filename,'rb')
-	header = struct.unpack(fmt,f.read(numBytes)),
+	header = struct.unpack(fmt,f.read(numBytes))
+	print header
 	buff = f.read()
 	f.close()
 	return header,buff
@@ -23,13 +24,13 @@ def _load_mnist(filenames):
 	labels_header,labels_buffer = _read_header(filenames[1],'>II',8)
 	labels = np.fromstring(labels_buffer,dtype=_labels_datatype(),count=labels_header[1])
 	images = np.fromstring(images_buffer,dtype=_images_datatype(),count=images_header[1]*images_header[2]*images_header[3]).reshape(images_header[1],images_header[2],images_header[3])
-	return data(X=images,Y=labels)
+	return _data(X=images,Y=labels)
 
 def traindataset():
-	return _load_mnist((os.path.abspath(os.path.join(datadir,'train-images-idx3-ubyte.gz')),
-		os.path.abspath(os.path.join(datadir,'train-labels-idx1-ubyte.gz'))))
+	return _load_mnist((os.path.abspath(os.path.join(_datadir,'train-images-idx3-ubyte.gz')),
+		os.path.abspath(os.path.join(_datadir,'train-labels-idx1-ubyte.gz'))))
 
 
 def testdataset():
-	return _load_mnist((os.path.abspath(os.path.join(datadir,'t10k-images-idx3-ubyte.gz')),
-		os.path.abspath(os.path.join(datadir,'t10k-labels-idx1-ubyte.gz'))))
+	return _load_mnist((os.path.abspath(os.path.join(_datadir,'t10k-images-idx3-ubyte.gz')),
+		os.path.abspath(os.path.join(_datadir,'t10k-labels-idx1-ubyte.gz'))))
