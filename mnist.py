akashmehra/cvml

commit f29db93a9a123965867591d9cb514b9323306b6b

reading into the buffer and closing the file.

mnist.py
@@ -8,7 +8,9 @@ datadir = os.path.join(curdir,'data')
 
 def _read_header(filename, fmt, numBytes):
 	f = gzip.GzipFile(filename,'rb')
-	return struct.unpack(fmt,f.read(numBytes)),f.read()
+	buff = f.read()
+	f.close()
+	return struct.unpack(fmt,f.read(numBytes)),
 
 def _labels_datatype():
 	return np.dtype('>b')

commit a5168bde71c7b1953a65a4ea9d8ef847c652e4ae

commit f29db93 introduced a bug where in reading numBytes would have returned empty string as read() was called before it.

mnist.py
@@ -7,9 +7,10 @@ datadir = os.path.join(os.path.dirname(__file__),'data')
 
 def _read_header(filename, fmt, numBytes):
 	f = gzip.GzipFile(filename,'rb')
+	header = struct.unpack(fmt,f.read(numBytes)),
 	buff = f.read()
 	f.close()
-	return struct.unpack(fmt,f.read(numBytes)),
+	return header,buff
 
 def _labels_datatype():
 	return np.dtype('>b')